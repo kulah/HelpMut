b61c0b1ceb254c723073ce16611c1b3dab794ff3
kulah <ibrahimkulah@gmail.com>
2015-07-02 14:27:55 +0300

    modal_popup_ayarlandı

--- login.php
+++ login.php
@@ -36,6 +36,10 @@ else {
     <script src="css/jquery.min.js"></script><style type="text/css"></style>
     <script src="css/bootstrap.min.js"></script>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script type="text/javascript">
         function acac(baslik, icerik) {
             $("#myModalLabel").html(baslik);
