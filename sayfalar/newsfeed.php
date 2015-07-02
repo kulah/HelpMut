66649bced797ae1d71f36124867998c5b7c7eb0a
kulah <ibrahimkulah@gmail.com>
2015-07-02 19:31:19 +0300

    timeline_yapıldı

--- sayfalar/newsfeed.php
+++ sayfalar/newsfeed.php
@@ -1,6 +1,55 @@
 <?php if (!defined('CONFIG_CLASS_INCLUDED')) {
   die(header("location: ../index.php"));
-}?>  
+}
+function turkcetarih($f, $zt = 'now'){
+  $z = date("$f", strtotime($zt));
+  $donustur = array(
+    'Monday'  => 'Pazartesi',
+    'Tuesday' => 'Salı',
+    'Wednesday' => 'Çarşamba',
+    'Thursday'  => 'Perşembe',
+    'Friday'  => 'Cuma',
+    'Saturday'  => 'Cumartesi',
+    'Sunday'  => 'Pazar',
+    'January' => 'Ocak',
+    'February'  => 'Şubat',
+    'March'   => 'Mart',
+    'April'   => 'Nisan',
+    'May'   => 'Mayıs',
+    'June'    => 'Haziran',
+    'July'    => 'Temmuz',
+    'August'  => 'Ağustos',
+    'September' => 'Eylül',
+    'October' => 'Ekim',
+    'November'  => 'Kasım',
+    'December'  => 'Aralık',
+    'Mon'   => 'Pts',
+    'Tue'   => 'Sal',
+    'Wed'   => 'Çar',
+    'Thu'   => 'Per',
+    'Fri'   => 'Cum',
+    'Sat'   => 'Cts',
+    'Sun'   => 'Paz',
+    'Jan'   => 'Oca',
+    'Feb'   => 'Şub',
+    'Mar'   => 'Mar',
+    'Apr'   => 'Nis',
+    'Jun'   => 'Haz',
+    'Jul'   => 'Tem',
+    'Aug'   => 'Ağu',
+    'Sep'   => 'Eyl',
+    'Oct'   => 'Eki',
+    'Nov'   => 'Kas',
+    'Dec'   => 'Ara',
+  );
+  foreach($donustur as $en => $tr){
+    $z = str_replace($en, $tr, $z);
+  }
+  if(strpos($z, 'Mayıs') !== false && strpos($f, 'F') === false) $z = str_replace('Mayıs', 'May', $z);
+  return $z;
+}
+
+?>  
 <form name="form1" method="post" action="" id="form1">
   <!-- Gelen Mesaj sonu -->
     <div class="box box-info direct-chat direct-chat-info">
@@ -16,6 +65,7 @@
 <?php
 if ($_POST){
 date_default_timezone_set('Europe/Istanbul');
+
 $sql = "INSERT INTO newsfeed (ICERIK,EKUL,ETAR) VALUES (:title,:user,:etard)";
 $q = $db->prepare($sql);
 $q->execute(array("user"  => $_SESSION["kullaniciid"],
@@ -25,26 +75,58 @@ header("location: index.php");
 }
 ?>
 </form>
+
+<!-- Gelen Mesaj -->
+<ul class="timeline">
 <?php                
-$sql = $db->query("SELECT n.ID, n.DURUM, n.ICERIK, k.ADSOYAD,n.ETAR, n.EKUL, n.DTAR, n.DKUL  FROM newsfeed n,kullanici k where k.ID=n.EKUL order by id desc");
+$sqltar = $db->query("SELECT  DATE_FORMAT(A.ETAR,'%M') AS ETAR FROM 
+                      (SELECT n.ID, n.DURUM, n.ICERIK, k.ADSOYAD,n.ETAR, n.EKUL, n.DTAR, n.DKUL  
+                              FROM newsfeed n,kullanici k where k.ID=n.EKUL) A
+                      GROUP BY  DATE_FORMAT(A.ETAR,'%M')");
+foreach ($sqltar as $timelinetarsql)
+{ ?>
+    <li class="time-label">
+        <span class="bg-red">
+        <?php echo turkcetarih('j F Y',$timelinetarsql['ETAR']);?>
+        </span>
+    </li>
+<?php $sql = $db->query("SELECT n.ID, n.DURUM, n.ICERIK, k.ADSOYAD,n.ETAR, n.EKUL, n.DTAR, n.DKUL  FROM newsfeed n,kullanici k where k.ID=n.EKUL  AND DATE_FORMAT(N.ETAR,'%M') = '".$timelinetarsql['ETAR']."' order by n.ETAR desc");
 foreach ($sql as $newsfeedsql)
 { ?>
 
+<ul class="timeline">
+    <!-- timeline tarih başı-->
 
-<!-- Gelen Mesaj -->
+    <!-- /.timeline-tarih sonu -->
 
-<div class="box box-solid" >
-    <div class="col-md-1">    
-             <img class="direct-chat-img" src="Content/img/user1-128x128.jpg" alt="message user image" />
-    </div>
-    <div class='box-header with-border'>
-        <span class='direct-chat-name pull-left'><?php echo $newsfeedsql['ADSOYAD'];?></span>
-        <span class='direct-chat-timestamp pull-right'><?php echo date("d/m H:i:s",strtotime($newsfeedsql['ETAR'])); ?></span>
-    </div >
-    <div class="box-body">
-        <p><?php echo $newsfeedsql['ICERIK'] ?></p>
-    </div>
-</div>
+    <!-- timeline item -->
+    <li>
+        <!-- timeline icon -->
+        <i class="fa fa-user bg-aqua"></i>
+        <div class="timeline-item">
+            <span class="time"><i class="fa fa-clock-o"></i> <?php echo date("H:i:s",strtotime($newsfeedsql['ETAR'])); ?></span>
+
+            <h3 class="timeline-header"><a href="#"><?php echo $newsfeedsql['ADSOYAD'];?></a> ...</h3>
+
+            <div class="timeline-body">
+                <?php echo $newsfeedsql['ICERIK'] ?>
+            </div>
+            <div class='timeline-footer'>
+                <a class="btn btn-primary btn-xs">Read more</a>
+                <a class="btn btn-danger btn-xs">Delete</a>
+                <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+            </div>
+        </div>
+    </li>
+    <!-- END timeline item -->
+</ul>
+
+
+
                     <?php
-                            }
+                            }}
+
                     ?>
+
+
+               
